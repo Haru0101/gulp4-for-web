2ca602eaedeb36e7c88f5155f5b58283d3f85766
Haruki Okuda <haruki.for.01@gmail.com>
2021-01-17 18:03:43 +0900

    autoprefixerの設定
    Signed-off-by: Haruki Okuda <haruki.for.01@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,10 @@ const sass = require("gulp-sass");
 // scssに変更があるたびにコンパイルを行う  
 gulp.task("watch-scss", function () {
     // 監視するファイル  
-    return gulp.watch("src/scss/common.scss", function () {
+    return gulp.watch("scss/common.scss", function () {
         return (
             gulp
-            .src("src/scss/common.scss")
+            .src("scss/common.scss")
             .pipe(
                 sass({
                     // 出力スタイルを指定
@@ -18,7 +18,23 @@ gulp.task("watch-scss", function () {
                 .on("error", sass.logError)
             )
             // コンパイル後のcssファイルの出力先  
-            .pipe(gulp.dest("pre-autoprefixer"))
+            .pipe(gulp.dest("pre-autoprefix"))
         );
     });
 });
+
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+
+gulp.task("autopre", function () {
+  return gulp.watch('pre-autoprefixer/common.css', function () {
+    return gulp.src("pre-autoprefixer/common.css")
+      .pipe(postcss([
+        autoprefixer({
+          cascade: false
+        })
+      ]))
+      .pipe(gulp.dest("css"));
+  });
+});
+
